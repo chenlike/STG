0563399f9e7ded1a8500d5c8d3332c305d201de0
chenlike <617682513@qq.com>
2018-02-06 22:37:31 +0800

    add img

---
+++ Assets/script/Danmu/Doremi.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DanmuLib;
+public class Doremi : BulletShooter {
+    GameObject tem;
+    Animator ator;
+    public void rreset()
+    {
+        ator.SetInteger("move", 0);
+    }
+    IEnumerator DanmuShootLoop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            List<GameObject> fireList = CircleDanmu.CreateCircle(tem, this.transform, 36, 1.66f);
+            Shoot(fireList);
+            ator.SetInteger("move", 3);
+        }
+
+    }
+    private void Start()
+    {
+        ator = GetComponent<Animator>();
+        tem = Resources.Load("bullet/RedBall") as GameObject;
+        StartCoroutine(DanmuShootLoop());
+    }
+}
